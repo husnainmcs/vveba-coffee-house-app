3a9aaa49506e2e19a62401d44ac134218e44e025
ali husnain <bakhtawaralishigri@gmail.com>
2025-08-04 18:34:26 +0500

    about data refactring

--- components/about.js
+++ components/about.js
@@ -1,32 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
-  return (
-   <section className="about">
-    <div>
-     <i className="fa-solid fa-mobile"></i>
-     <h1>Easy to Order</h1>
+ const aboutData = [
+  {
+   id: 1,
+   icon: <i className="fa-solid fa-mobile"></i>,
+   title: 'Easy to Order',
+   des: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ut suscipit, quod quidem cumque rerum.',
+  },
+  {
+   id: 2,
+   icon: <i className="fa-solid fa-truck"></i>,
+   title: 'Fastest Delivery',
+   des: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ut suscipit, quod quidem cumque rerum.',
+  },
+  {
+   id: 3,
+   icon: <i className="fa-solid fa-mug-hot"></i>,
+   title: 'Quality Coffee',
+   des: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ut suscipit, quod quidem cumque rerum.',
+  },
+ ];
+ return (
+  <section className="about">
+   {aboutData.map((data) => (
+    <div key={data.id}>
+     {data.icon}
+     <h1>{data.title}</h1>
      <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ut
-      suscipit, quod quidem cumque rerum.
+     {data.des}
      </p>
     </div>
-    <div>
-     <i className="fa-solid fa-truck"></i>
-     <h1>Fastest Delivery</h1>
-     <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ut
-      suscipit, quod quidem cumque rerum.
-     </p>
-    </div>
-    <div>
-     <i className="fa-solid fa-mug-hot"></i>
-     <h1>Quality Coffee</h1>
-     <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ut
-      suscipit, quod quidem cumque rerum.
-     </p>
-    </div>
-   </section>
-  );
+   ))}
+  </section>
+ );
 }
